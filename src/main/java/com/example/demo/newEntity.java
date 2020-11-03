855f6b4701995c509d8651f1ce3df87a22779e9c
MichailMastanov <mixleb030583@gmail.com>
2020-11-03 22:44:23 +0300

    entity

---
+++ src/main/java/com/example/demo/newEntity.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+public class newEntity {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "newEntity{" +
+                "id=" + id +
+                '}';
+    }
+}
